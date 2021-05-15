6352d01fafc22f33a4684fe6632df388947dbb5a
Rez22 <asif.re@hotmail.com>
2021-05-15 12:42:42 +0200

    End of section 8

---
+++ API/DTOs/MemberDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+namespace API.DTOs
+{
+    public class MemberDto
+    {
+        
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PhotoUrl { get; set; }
+        public int Age { get; set; }
+        public string KnownAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LasteActive { get; set; }
+        public string Gender { get; set; }
+        public string LookingFor { get; set; }
+        public string Introdution { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<PhotoDto> Photos { get; set; }
+    }
+}
